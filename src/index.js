a7216ad52b995e3b5eb264e51dde08668e8702ff
maxim7137 <maxim713@mail.ru>
2022-11-14 21:23:26 +0300

    pages: HashRouter

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
+  <HashRouter>
     <App />
-  </Router>
+  </HashRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
